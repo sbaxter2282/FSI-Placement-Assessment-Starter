7296c1fec507e94cd3ecd4a3017a02f079e04925
sbaxter <sbaxter2282@gmail.com>
2021-12-01 20:02:57 -0600

    Add my changes to file

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,11 @@ console.log('JavaScript code has loaded!')
 let yourName = "Steve Baxter" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let gingerbreadQty = 0      // Gingerbread
+let chocolateChipQty = 0      // Chocolate Chip
+let sugarSprinkleQty = 0   // Sugar Sprinkle
+let totalQty = 0
+
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
@@ -15,108 +17,82 @@ document.getElementById('credit').textContent = `Created by ${yourName}`
 
 document.getElementById('add-gb').addEventListener('click', function() {
         console.log('Gingerbread + button was clicked!')
+
+    gingerbreadQty = gingerbreadQty +1;
+    document.getElementById('qty-gb').textContent = gingerbreadQty
+   
+    totalQty = gingerbreadQty+chocolateChipQty+sugarSprinkleQty
+
+    document.getElementById('qty-total').textContent = totalQty
+
     })
-
-
-let gingerbreadQtyCell = document.querySelector("#qty-gb");
-let gingerbreadQty = localStorage.getItem('gingerbreadQty')
-if (gingerbreadQty) {
-    gingerbreadQtyCell.textContent = gingerbreadQty;
-}
-
  
-document.getElementById('add-gb').addEventListener('click', function(e) {
-    let gingerbreadQtyCount = parseInt(gingerbreadQtyCell.textContent);
-    gingerbreadQtyCount = gingerbreadQtyCount + 1;
-    gingerbreadQtyCell.textContent = gingerbreadQtyCount;
-    localStorage.setItem('qty-gb', gingerbreadQty);
-});
-
 
 document.getElementById('minus-gb').addEventListener('click', function() {
      console.log('Gingerbread - button was clicked!')
-})
 
- 
-document.getElementById('minus-gb').addEventListener('click', function(e) {
-    let gingerbreadQtyCount = parseInt(gingerbreadQtyCell.textContent);
-    gingerbreadQtyCount = gingerbreadQtyCount - 1;
-    gingerbreadQtyCell.textContent = gingerbreadQtyCount;
-    localStorage.setItem('qty-gb', gingerbreadQty);
-});
+
+
+gingerbreadQty = gingerbreadQty -1;
+document.getElementById('qty-gb').textContent = gingerbreadQty
+
+totalQty = gingerbreadQty+chocolateChipQty+sugarSprinkleQty
+
+document.getElementById('qty-total').textContent = totalQty
+
+
+})
 
 
 document.getElementById('add-cc').addEventListener('click', function() {
-        console.log('Chocolate Chip + button was clicked!')
-  })
+    console.log('Chocolate Chip + button was clicked!')
 
+    chocolateChipQty = chocolateChipQty +1;
+document.getElementById('qty-cc').textContent = chocolateChipQty
 
-let chocolateChipQtyCell = document.querySelector("#qty-cc");
-let chocolateChipQty = localStorage.getItem('chocolateChipQty')
-if (chocolateChipQty) {
-    chocolateChipQtyCell.textContent = chocolateChipQty;
-}
+totalQty = gingerbreadQty+chocolateChipQty+sugarSprinkleQty
 
- 
-document.getElementById('add-cc').addEventListener('click', function(e) {
-    let chocolateChipQtyCount = parseInt(chocolateChipQtyCell.textContent);
-    chocolateChipQtyCount = chocolateChipQtyCount + 1;
-    chocolateChipQtyCell.textContent = chocolateChipQtyCount;
-    localStorage.setItem('qty-cc', chocolateChipQty);
-});
+document.getElementById('qty-total').textContent = totalQty
+
+})
 
 
 document.getElementById('minus-cc').addEventListener('click', function() {
-        console.log('Chocolate Chip - button was clicked!')
+    console.log('Chocolate Chip - button was clicked!')
+
+    chocolateChipQty = chocolateChipQty -1;
+document.getElementById('qty-cc').textContent = chocolateChipQty
+
+totalQty = gingerbreadQty+chocolateChipQty+sugarSprinkleQty
+
+document.getElementById('qty-total').textContent = totalQty
+
 })
-
-
-document.getElementById('minus-cc').addEventListener('click', function(e) {
-    let chocolateChipQtyCount = parseInt(chocolateChipQtyCell.textContent);
-    chocolateChipQtyCount = chocolateChipQtyCount - 1;
-    chocolateChipQtyCell.textContent = chocolateChipQtyCount;
-    localStorage.setItem('qty-cc', chocolateChipQty);
-});
 
 
 document.getElementById('add-sugar').addEventListener('click', function() {
-       console.log('Sugar Sprinkle + button was clicked!')
+    console.log('Sugar Sprinkle Cookie + button was clicked!')
+
+    sugarSprinkleQty = sugarSprinkleQty +1;
+document.getElementById('qty-sugar').textContent = sugarSprinkleQty
+
+totalQty = gingerbreadQty+chocolateChipQty+sugarSprinkleQty
+
+document.getElementById('qty-total').textContent = totalQty
+
 })
-
-
-let sugarSprinkleQtyCell = document.querySelector("#qty-sugar");
-let sugarSprinkleQty = localStorage.getItem('sugarSprinkleQty')
-if (sugarSprinkleQty) {
-    sugarSprinkleQtyCell.textContent = sugarSprinkleQty;
-}
-
- 
-document.getElementById('add-sugar').addEventListener('click', function(e) {
-    let sugarSprinkleQtyCount = parseInt(sugarSprinkleQtyCell.textContent);
-    sugarSprinkleQtyCount = sugarSprinkleQtyCount + 1;
-    sugarSprinkleQtyCell.textContent = sugarSprinkleQtyCount;
-    localStorage.setItem('qty-sugar', sugarSprinkleQty);
-});
 
 
 document.getElementById('minus-sugar').addEventListener('click', function() {
-       console.log('Sugar Sprinkle - button was clicked!')
+    console.log('Sugar Sprinkle Cookie - button was clicked!')
+
+    sugarSprinkleQty = sugarSprinkleQty -1;
+document.getElementById('qty-sugar').textContent = sugarSprinkleQty
+
+totalQty = gingerbreadQty+chocolateChipQty+sugarSprinkleQty
+
+document.getElementById('qty-total').textContent = totalQty
+
 })
-
-
-document.getElementById('minus-sugar').addEventListener('click', function(e) {
-    let sugarSprinkleQtyCount = parseInt(sugarSprinkleQtyCell.textContent);
-    sugarSprinkleQtyCount = sugarSprinkleQtyCount - 1;
-    sugarSprinkleQtyCell.textContent = sugarSprinkleQtyCount;
-    localStorage.setItem('qty-sugar', sugarSprinkleQty);
-});
-
-
-let summary = document.querySelectorAll(".summary")
-console.log(summary)
-
-
-
-
 
 
